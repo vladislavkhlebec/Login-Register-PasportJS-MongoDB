d08959ad6ac7776ba7447b3429023448be8e4d81
Vladislav <vladislavkhlebec@gmail.com>
2020-04-10 09:51:27 +0300

    implement base functionality

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuthenticated }  = require('../config/auth')
+//Welvome Page
+router.get('/', (req, res) => res.render('welcome'))
+//Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+	user: req.user
+}))
+module.exports = router
